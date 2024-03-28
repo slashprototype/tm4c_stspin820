59ef1d20794f3ca8ab857e9435a18c32fdc0b624
l.juarez <l.juarez@posgrado.cidesi.edu.mx>
2024-03-27 19:40:02 -0600

    timer module added

--- stspin820_driver.c
+++ stspin820_driver.c
@@ -5,39 +5,63 @@
  *      Author: JUARELU1
  */
 
-#include <drivers/stspin820/stspin820_driver.h>
-#include <stdint.h>
+#include "drivers/tm4c_stspin820/stspin820_driver.h"
 #include <stdbool.h>
+#include <stdint.h>
 #include <tm4c_utils/pins.h>
+#include "tm4c_utils/timers.h"
 
-
-void configStepMode(stspin820_driver_t* driver, step_mode_t step_mode){
-    pinWrite(driver->m1, step_mode & 0b0001);
-    pinWrite(driver->m2, (step_mode & 0b0010) >> 1);
-    pinWrite(driver->m3, (step_mode & 0b0100) >> 2); 
+static void configureSpeed(stspin820_driver_t* driver, uint32_t frequency_hz) {
+    uint32_t some_speed_relational = frequency_hz;
+    driver->speed_ml_min = some_speed_relational;
+    driver->timer->configureFrequency(driver->timer, driver->speed_ml_min);
 }
 
-void enableStepMotor(stspin820_driver_t* driver, uint8_t is_forward){
-    pinWrite(driver->pin_en, 0);
+static void runForward(stspin820_driver_t* driver, uint32_t frequency_hz) {
+    driver->enable(driver);
+    driver->configureSpeed(driver, frequency_hz);
+    driver->timer->enable(driver->timer);
+    pinWrite(driver->pin_dir, FORWARD);
+}
+
+static void runBackward(stspin820_driver_t* driver, uint32_t frequency_hz) {
+    driver->enable(driver);
+    driver->configureSpeed(driver, frequency_hz);
+    driver->timer->enable(driver->timer);
+    pinWrite(driver->pin_dir, BACKWARD);
+}
+
+static void enable(stspin820_driver_t* driver) {
+    pinWrite(driver->pin_en, 1);
     pinWrite(driver->pin_stby, 1);
-    
-    if(is_forward){
-        pinWrite(driver->pin_dir, 1);
-    }
-    else{
-        pinWrite(driver->pin_dir, 0);
-    }
 }
 
-void disableStepMotor(stspin820_driver_t* driver){
+static void disable(stspin820_driver_t* driver) {
     pinWrite(driver->pin_en, 1);
     pinWrite(driver->pin_stby, 0);
+    driver->timer->disable(driver->timer);
 }
 
-void setStandByStepMotor(stspin820_driver_t* driver){
+static void standBy(stspin820_driver_t* driver) {
     pinWrite(driver->pin_en, 0);
     pinWrite(driver->pin_stby, 1);
+    driver->timer->disable(driver->timer);
 }
 
+static void configStepMode(stspin820_driver_t* driver, step_mode_t step_mode) {
+    pinWrite(driver->m1, step_mode & 0b0001);
+    pinWrite(driver->m2, (step_mode & 0b0010) >> 1);
+    pinWrite(driver->m3, (step_mode & 0b0100) >> 2);
+}
 
+void setupStepMotor(stspin820_driver_t* driver, step_mode_t step_mode) {
+    driver->runForward = &runForward;
+    driver->runBackward = &runBackward;
+    driver->enable = &enable;
+    driver->disable = &disable;
+    driver->standBy = &standBy;
+    driver->configStepMode = &configStepMode;
+    driver->configureSpeed = &configureSpeed;
 
+    driver->configStepMode(driver, step_mode);
+}
--- stspin820_driver.h
+++ stspin820_driver.h
@@ -8,35 +8,33 @@
 #ifndef SRC_DRIVERS_STSPIN820_STSPIN820_DRIVER_H_
 #define SRC_DRIVERS_STSPIN820_STSPIN820_DRIVER_H_
 /// \cond
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
 /// \endcond
 #include <tm4c_utils/pins.h>
+#include "tm4c_utils/timers.h"
 
 /* Step mode pre-scaler selection*/
 typedef enum {
-    STEP_1_1   = 0b0000,
-    STEP_1_2   = 0b0001,
-    STEP_1_4   = 0b0010,
-    STEP_1_8   = 0b0011,
-    STEP_1_16  = 0b0100,
-    STEP_1_32  = 0b0101,
+    STEP_1_1 = 0b0000,
+    STEP_1_2 = 0b0001,
+    STEP_1_4 = 0b0010,
+    STEP_1_8 = 0b0011,
+    STEP_1_16 = 0b0100,
+    STEP_1_32 = 0b0101,
     STEP_1_128 = 0b0110,
     STEP_1_256 = 0b0111,
-}step_mode_t;
+} step_mode_t;
 
-typedef enum {
-    BACKWARD = 0U,
-    FORWARD = 1U
-}dir_mode_t;
+typedef enum { BACKWARD = 0U, FORWARD = 1U } dir_mode_t;
 
 /* Legacy functions to handle normal driver operations*/
-typedef struct stspin820_legacy_functions stspin820_legacy_functions_t;
+typedef struct stspin820_driver stspin820_driver_t;
 
 /* 
  * Library - driver base structure
  */
-typedef struct{
+struct stspin820_driver {
     pin_t* pin_en;
     pin_t* pin_dir;
     pin_t* pin_step;
@@ -45,26 +43,18 @@ typedef struct{
     pin_t* m2;
     pin_t* m3;
     uint16_t step_mode;
-    void (*runForward)(uint32_t);
-    void (*runBackward)(uint32_t);
-    stspin820_legacy_functions_t* legacy_functions;
-
-}stspin820_driver_t;
-
-struct stspin820_legacy_functions {
-    void (*enable)(stspin820_driver_t*, dir_mode_t);
+    uint32_t speed_ml_min;
+    timer_module_t* timer;
+    void (*runForward)(stspin820_driver_t*, uint32_t);
+    void (*runBackward)(stspin820_driver_t*, uint32_t);
+    void (*enable)(stspin820_driver_t*);
     void (*disable)(stspin820_driver_t*);
-    void (*setStandBy)(stspin820_driver_t*);
+    void (*standBy)(stspin820_driver_t*);
+    void (*configStepMode)(stspin820_driver_t*, step_mode_t);
+    void (*configureSpeed)(stspin820_driver_t*, uint32_t);
 };
 
-void configStepMode(stspin820_driver_t* driver, step_mode_t step_mode);
-
-/* Function to enable the step motor in the direction selected
- * @param is_forward set the @ref pin_dir in the selected configuration, 
- * use the @ref dir_mode_t to select backward or forward
- */
-void enableStepMotor(stspin820_driver_t* driver, dir_mode_t is_forward);
-void disableStepMotor(stspin820_driver_t* driver);
-void setStandByStepMotor(stspin820_driver_t* driver);
+/* Assign local functions and another initial local configuration*/
+void setupStepMotor(stspin820_driver_t* driver, step_mode_t step_mode);
 
 #endif /* SRC_DRIVERS_STSPIN820_STSPIN820_DRIVER_H_ */
